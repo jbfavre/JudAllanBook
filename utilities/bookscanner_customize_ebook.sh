5185ffc780f6378bcd0457bab8794ac7b8d11a26
Jean Baptiste Favre <github@jbfavre.org>
2014-08-25 17:04:26 +0200

    Update utilities

--- utilities/bookscanner_customize_ebook.sh
+++ utilities/bookscanner_customize_ebook.sh
@@ -11,5 +11,8 @@ WIDTH=$(convert $1/cover-image.jpg -print "%w\n" /dev/null)
 HEIGHT=$(convert $1/cover-image.jpg -print "%h\n" /dev/null)
 sed -i "s|<div.*|<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='100%' height='100%' viewBox='0 0 ${WIDTH} ${HEIGHT}' preserveAspectRatio='xMidYMid meet'><image width='${WIDTH}' height='${HEIGHT}' xlink:href='cover-image.jpg' /></svg>|g;s|<img.*||g;s|</div>||g" $1/cover.xhtml
 
-sed -i 's|<itemref idref="title_page_xhtml" linear="no" />|<itemref idref="title_page_xhtml" linear="yes" />|g' $1/content.opf
+sed -i 's|<item id="title_page_xhtml" href="title_page.xhtml" media-type="application/xhtml+xml" />|<item id="title_page_xhtml" href="title_page.xhtml" media-type="application/xhtml+xml" />\n    <item id="preface_xhtml" href="preface.xhtml" media-type="application/xhtml+xml" />|g' $1/content.opf
+sed -i 's|<itemref idref="title_page_xhtml" linear="no" />|<itemref idref="title_page_xhtml" linear="yes" />\n    <itemref idref="preface_xhtml" linear="yes" />|g' $1/content.opf
 sed -i 's|<item id="cover_xhtml" href="cover.xhtml" media-type="application/xhtml+xml" />|<item id="cover_xhtml" href="cover.xhtml" media-type="application/xhtml+xml" properties="svg" />|g' $1/content.opf
+
+sed -i 's|<style type="text/css">code{white-space: pre;}</style>|<link rel="stylesheet" type="text/css" href="stylesheet.css" />|g' $1/preface.xhtml
